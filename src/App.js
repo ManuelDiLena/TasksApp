b1e478f8bf0fe249cffc62b3255de9f6ffe873f3
ManuelDiLena <manuel.dilena.29@gmail.com>
2020-10-28 20:26:20 -0300

    TasksApp-React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { todos } from './todos.json';
+import TasksForm from './components/TasksForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { 
+            todos
+        }
+        this.handleAddTasks = this.handleAddTasks.bind(this)
+    }
+
+    removeTasks(index) {
+        this.setState ({
+            todos: this.state.todos.filter((e, i) => {
+                return i !== index
+            })
+        })
+    }
+
+    handleAddTasks(todo) {
+        this.setState ({
+            todos: [...this.state.todos, todo]
+        })
+    }
+
+    render() {
+        const todos = this.state.todos.map((todo, i) => {
+            return (
+                <div className="col-md-4" key={i}>
+                    <div className="card mt-4">
+                        <div className="card-title text-center">
+                            <h3>{todo.title}</h3>
+                            <span className="badge badge-pill badge-danger ml-2">{todo.priority}</span>
+                        </div>
+                        <div className="card-body">
+                            <p>{todo.description}</p>
+                            <p>{todo.responsible}</p>
+                        </div>
+                        <div className="card-footer">
+                            <button className="btn btn-danger" onClick={this.removeTasks.bind(this, i)}>
+                                Eliminar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+
+        return (
+            <div className="App">
+                <nav className="navbar navbar-dark bg-dark">
+                    <a className="text-white">
+                        Tareas
+                        <span className="badge badge-pill badge-light ml-2">
+                            { this.state.todos.length} 
+                        </span>
+                    </a>
+                </nav>
+                <div className="container">
+                    <div className="row mt-4">
+                        <div className="col-md-4 text-center">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <TasksForm onAddTasks={this.handleAddTasks}></TasksForm>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="row">
+                                { todos }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        );
+    }
 }
 
 export default App;
+
